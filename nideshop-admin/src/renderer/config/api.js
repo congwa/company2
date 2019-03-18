14cf4a7874adf17d3f695bbad46f4cd8f05305b9
wangcong <wangcong1112@chukong-inc.com>
2019-03-18 14:07:14 +0800

    up

--- nideshop-admin/src/renderer/config/api.js
+++ nideshop-admin/src/renderer/config/api.js
@@ -1,6 +1,6 @@
 //const rootUrl = 'http://127.0.0.1:8360/admin/';
 const rootUrl = 'http://139.199.103.116:8360/admin/'
-const imgUrl = 'http://139.199.103.116:8360/'
+const imgUrl = 'http://139.199.103.116:8360'
 
 const api = {
 	rootUrl:rootUrl,
