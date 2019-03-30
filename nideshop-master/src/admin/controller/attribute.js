7b1bacd630485941a13737fc50a5aacaeee9621b
wangcong <cong_wa@163.com>
2019-03-30 22:15:41 +0800

    up

--- nideshop-master/src/admin/controller/attribute.js
+++ nideshop-master/src/admin/controller/attribute.js
@@ -20,6 +20,28 @@ module.exports = class extends think.Controller {
     return this.success(categoryList);
   }
 
+  async indexlistAction() {
+    const attribute_category = await this.model('attribute_category').where().select();
+    const data = await this.model('attribute').where().select();
+    const categoryList = [];
+    attribute_category.map((item) => {
+      let obj = {};
+      obj.label = item.name;
+      obj.value = item.id;
+      obj.children = [];
+      data.map( (child) => {
+        if(child.attribute_category_id === item.id) {
+          let ob = {};    
+          ob.value = child.id;
+          ob.label = child.name;
+          obj.children.push(ob);
+        }
+      })
+      categoryList.push(obj);
+    })
+    return this.success(categoryList);
+  }
+
   /**
    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
    `attribute_category_id` int(11) unsigned NOT NULL DEFAULT '0',
@@ -57,6 +79,14 @@ module.exports = class extends think.Controller {
 
     const model = this.model('attribute');
     const data = await model.where({id: id}).find();
+    return this.success(data);
+  }
+
+  async infogoodsAction() {
+    const id = this.post('id') || this.get('id');
+
+    const model = this.model('goods_attribute');
+    const data = await model.field('attribute_id,value').where({goods_id: id}).select();
     return this.success(data);
   }
 
--- nideshop-master/src/admin/controller/goods.js
+++ nideshop-master/src/admin/controller/goods.js
@@ -38,23 +38,27 @@ module.exports = class extends Base {
     values.is_new = values.is_new ? 1 : 0;
     values.is_hot = values.is_hot ? 1 : 0;
     values.is_delete = values.is_delete ? 1 : 0;  // 0代表上架  1代表下架
+    var v;
     if (id > 0) {
       await model.where({id: id}).update(values);
     } else {
       delete values.id;
-      await model.add(values);
+      v = await model.add(values);
+      id = v;
     }
+    console.log('-------------------------',v);
     //添加goods_gallery
     const gallery_list = values['gallery'];
-    await this.model('goods_gallery').where({id:id}).delete();
+    await this.model('goods_gallery').where({goods_id:id}).delete();
     const list = gallery_list.map((url ,index)=> {
       return {img_url:url,goods_id:id,img_desc:'',sort_order:index}
     })
+  
     await this.model('goods_gallery').addMany(list);
 
     //添加商品属性
     const goods_attribute = values['attribute'];
-    await this.model('goods_attribute').where({id:id}).delete();
+    await this.model('goods_attribute').where({goods_id:id}).delete();
     const a_list = goods_attribute.map(item => {
       item.goods_id = id;
       return item;
@@ -72,5 +76,21 @@ module.exports = class extends Base {
     // TODO 删除图片
 
     return this.success();
+  }
+
+  async infogoodsAction() {
+    const id = this.post('id') || this.get('id');
+
+    const model = this.model('goods_attribute');
+    const data = await model.field('attribute_id,value,attribute_category_id').where({goods_id: id}).join('nideshop_attribute ON nideshop_attribute.id=nideshop_goods_attribute.attribute_id').select();
+    return this.success(data);
+  }
+
+  async galleryinfoAction() {
+    const id = this.post('id');
+    const data = await this.model('goods_gallery').field('img_url').where({goods_id: id}).select();
+    // TODO 删除图片
+
+    return this.success(data);
   }
 };
