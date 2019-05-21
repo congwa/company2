d806fab863a0f9a86692febfd9ca84fa4678a1a4
wangcong <wangcong1112@chukong-inc.com>
2019-05-21 11:20:38 +0800

    up

--- nideshop-master/src/api/controller/index.js
+++ nideshop-master/src/api/controller/index.js
@@ -1,24 +1,51 @@
-const Base = require('./base.js');
+const Base = require("./base.js");
 
 module.exports = class extends Base {
   async indexAction() {
-    const banner = await this.model('ad').where({ad_position_id: 1}).select();
-    const channel = await this.model('channel').order({sort_order: 'asc'}).select();
-    const newGoods = await this.model('goods').field(['id', 'name', 'list_pic_url', 'retail_price']).where({is_new: 1}).limit(4).select();
-    const hotGoods = await this.model('goods').field(['id', 'name', 'list_pic_url', 'retail_price', 'goods_brief']).where({is_hot: 1}).limit(3).select();
-    const brandList = await this.model('brand').where({is_new: 1}).order({new_sort_order: 'asc'}).limit(4).select();
-    const topicList = await this.model('topic').limit(3).select();
+    const banner = await this.model("ad")
+      .where({ ad_position_id: 1 })
+      .select();
+    const channel = await this.model("channel")
+      .order({ sort_order: "asc" })
+      .select();
+    const newGoods = await this.model("goods")
+      .field(["id", "name", "list_pic_url", "retail_price"])
+      .where({ is_new: 1 })
+      .limit(4)
+      .select();
+    const hotGoods = await this.model("goods")
+      .field(["id", "name", "list_pic_url", "retail_price", "goods_brief"])
+      .where({ is_hot: 1 })
+      .limit(3)
+      .select();
+    const brandList = await this.model("brand")
+      .where({ is_new: 1 })
+      .order({ new_sort_order: "asc" })
+      .limit(4)
+      .select();
+    const topicList = await this.model("topic")
+      .limit(3)
+      .select();
 
-    const categoryList = await this.model('category').where({parent_id: 0, name: ['<>', '推荐']}).select(); // 找出所有的一级分类
+    const categoryList = await this.model("category")
+      .where({ parent_id: 0, name: ["<>", "推荐"] })
+      .select(); // 找出所有的一级分类
+    think.logger.debug("数据库查询完成");
     const newCategoryList = [];
-    // console.log('categoryList: ',categoryList);
     for (const categoryItem of categoryList) {
-      const childCategoryIds = await this.model('category').where({parent_id: categoryItem.id}).getField('id', 100); // 找出这个一级分类对应的子分类
+      const childCategoryIds = await this.model("category")
+        .where({ parent_id: categoryItem.id })
+        .getField("id", 100); // 找出这个一级分类对应的子分类
       let categoryGoods;
-      if (childCategoryIds.length === 0) { // 当父分类下面没有子分类的情况
+      if (childCategoryIds.length === 0) {
+        // 当父分类下面没有子分类的情况
         categoryGoods = [];
       } else {
-        categoryGoods = await this.model('goods').field(['id', 'name', 'list_pic_url', 'retail_price']).where({category_id: ['IN', childCategoryIds]}).limit(7).select(); // 把这些子分类的详情找出来
+        categoryGoods = await this.model("goods")
+          .field(["id", "name", "list_pic_url", "retail_price"])
+          .where({ category_id: ["IN", childCategoryIds] })
+          .limit(7)
+          .select(); // 把这些子分类的详情找出来
       }
       newCategoryList.push({
         id: categoryItem.id,
@@ -26,7 +53,7 @@ module.exports = class extends Base {
         goodsList: categoryGoods
       });
     }
-
+    think.logger.debug("遍历查询完成");
     return this.success({
       banner: banner,
       channel: channel, // 首页商品显示类型
