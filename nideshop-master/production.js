25d162d15b8060184e8b8820c1a2542f9b7fae91
wangcong <wangcong1112@chukong-inc.com>
2019-06-06 12:05:13 +0800

    up

--- nideshop-master/production.js
+++ nideshop-master/production.js
@@ -1,8 +1,16 @@
+
 const Application = require('thinkjs');
+const babel = require('think-babel');
+const watcher = require('think-watcher');
+const notifier = require('node-notifier');
 
 const instance = new Application({
   ROOT_PATH: __dirname,
-  proxy: true, // use proxy
+  watcher: watcher,
+  transpiler: [babel, {
+    presets: ['think-node']
+  }],
+  notifier: notifier.notify.bind(notifier),
   env: 'production'
 });
 
